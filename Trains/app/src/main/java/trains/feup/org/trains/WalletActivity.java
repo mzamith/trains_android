d1427a33002d748c854189927a03168ff3b292c7
Manuel Zamith <mzamith10@gmail.com>
2017-04-13 21:43:50 +0100

    Created wallet list adapter

--- Trains/app/src/main/java/trains/feup/org/trains/WalletActivity.java
+++ Trains/app/src/main/java/trains/feup/org/trains/WalletActivity.java
@@ -1,21 +1,30 @@
 package trains.feup.org.trains;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 import trains.feup.org.trains.api.ServerListCallback;
 import trains.feup.org.trains.api.ServerObjectCallback;
@@ -24,6 +33,8 @@ import trains.feup.org.trains.model.Travel;
 import trains.feup.org.trains.service.TicketService;
 import trains.feup.org.trains.service.TripService;
 import trains.feup.org.trains.util.JsonUtil;
+
+import static trains.feup.org.trains.TrainsApp.getContext;
 
 public class WalletActivity extends DrawerActivity {
 
@@ -41,8 +52,22 @@ public class WalletActivity extends DrawerActivity {
         drawer.addView(contentView, 0);
 
         ticketList = (ListView) findViewById(R.id.tickets_list);
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+        adapter = new TicketsAdapter(new ArrayList<Ticket>());
         ticketList.setAdapter(adapter);
+
+        ticketList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?>  adapter, View v, int position, long id){
+
+                //TODO CONNECT WITH TICKET DETAIL!!
+
+                Ticket item = (Ticket) adapter.getItemAtPosition(position);
+                Toast.makeText(WalletActivity.this, "Clicked on item" + item.getCode(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+
 
         checkConnectionErrors();
 
@@ -53,12 +78,14 @@ public class WalletActivity extends DrawerActivity {
             @Override
             public void OnSuccess(JSONArray result) {
 
+                adapter.clear();
+
                 Gson gson = new Gson();
                 Ticket[] ticketsArray = gson.fromJson(result.toString(), Ticket[].class);
                 tickets = new ArrayList<>(Arrays.asList(ticketsArray));
 
+                adapter.notifyDataSetChanged();
                 adapter.addAll(tickets);
-
             }
 
             @Override
@@ -78,5 +105,56 @@ public class WalletActivity extends DrawerActivity {
                 Toast.makeText(this, "Make sure you have Wifi Connection", Toast.LENGTH_LONG).show();
                 break;
         }
+    }
+
+
+    public class TicketsAdapter extends ArrayAdapter<Ticket> {
+
+        public TicketsAdapter(List<Ticket> items) {
+            super(WalletActivity.this, R.layout.ticket_row, items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View v = convertView;
+
+            if (v == null) {
+                LayoutInflater vi;
+                vi = LayoutInflater.from(getContext());
+                v = vi.inflate(R.layout.ticket_row, null);
+            }
+
+            final Ticket ticket = getItem(position);
+
+            if (ticket != null) {
+                View colorBar = v.findViewById(R.id.color_ticket);
+                TextView departure = (TextView) v.findViewById(R.id.ticket_departure);
+                TextView arrival = (TextView) v.findViewById(R.id.ticket_arrival);
+                TextView date = (TextView) v.findViewById(R.id.ticket_day);
+
+                departure.setText(ticket.getFrom().getLabel());
+                arrival.setText(ticket.getTo().getLabel());
+
+                DateFormat df = new SimpleDateFormat("dd/MM");
+                date.setText(df.format(new Date(ticket.getDay())));
+
+                switch (ticket.getState()){
+                    case "RESERVED":
+                        colorBar.setBackgroundColor(getColor(R.color.amber_700));
+                        break;
+                    case "DENIED":
+                        colorBar.setBackgroundColor(getColor(R.color.red_700));
+                        break;
+                    case "VALIDATED":
+                        colorBar.setBackgroundColor(getColor(R.color.green_700));
+                        break;
+                }
+
+            }
+
+            return v;
+        }
+
     }
 }
