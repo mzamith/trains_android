473de9b3618a30f660889ce15a52634389aa201d
Leonardo Machado <leonardo.ccmachado@gmail.com>
2017-04-20 19:59:31 +0100

    The app does not crash when loading tickets that were not bought on this phone

--- Trains/app/src/main/java/trains/feup/org/trains/TicketDetailActivity.java
+++ Trains/app/src/main/java/trains/feup/org/trains/TicketDetailActivity.java
@@ -1,5 +1,6 @@
 package trains.feup.org.trains;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteAbortException;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,11 +10,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import trains.feup.org.trains.exception.ApiException;
 import trains.feup.org.trains.storage.TicketsContract;
 import trains.feup.org.trains.storage.TicketsDbHelper;
 
@@ -41,7 +44,7 @@ public class TicketDetailActivity extends AppCompatActivity {
     ImageView qrCodeImageview;
     String qrContent;
     String errorMsg = "";
-    public final static int WIDTH=200;
+    public final static int WIDTH = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,40 +52,51 @@ public class TicketDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ticket_detail);
 
         TicketsDbHelper ticketsDbHelper = new TicketsDbHelper(getContext());
-        try (SQLiteDatabase db = ticketsDbHelper.getReadableDatabase()) {
-            Cursor cursor;
+        SQLiteDatabase db = ticketsDbHelper.getReadableDatabase();
+        Cursor cursor;
 
-            String[] projection = {
-                    TicketsContract.TicketEntry.COLUMN_ID,
-                    TicketsContract.TicketEntry.COLUMN_DATE,
-                    TicketsContract.TicketEntry.COLUMN_DEPARTURE,
-                    TicketsContract.TicketEntry.COLUMN_DEPARTURE_TIME,
-                    TicketsContract.TicketEntry.COLUMN_DESTINATION,
-                    TicketsContract.TicketEntry.COLUMN_PRICE,
-                    TicketsContract.TicketEntry.COLUMN_STATE,
-                    TicketsContract.TicketEntry.COLUMN_CODE_DTO
-            };
+        String[] projection = {
+                TicketsContract.TicketEntry.COLUMN_ID,
+                TicketsContract.TicketEntry.COLUMN_DATE,
+                TicketsContract.TicketEntry.COLUMN_DEPARTURE,
+                TicketsContract.TicketEntry.COLUMN_DEPARTURE_TIME,
+                TicketsContract.TicketEntry.COLUMN_DESTINATION,
+                TicketsContract.TicketEntry.COLUMN_PRICE,
+                TicketsContract.TicketEntry.COLUMN_STATE,
+                TicketsContract.TicketEntry.COLUMN_CODE_DTO
+        };
 
-            String selection = TicketsContract.TicketEntry.COLUMN_ID + " = ?";
+        String selection = TicketsContract.TicketEntry.COLUMN_ID + " = ?";
 
-            long TicketID = getIntent().getExtras().getLong("Ticket_ID");
-            id = String.valueOf(TicketID);
+        long TicketID = getIntent().getExtras().getLong("Ticket_ID");
+        id = String.valueOf(TicketID);
 
-            String[] selectionArgs = {id};
+        String[] selectionArgs = {id};
 
-            String sortOrder = TicketsContract.TicketEntry.COLUMN_ID + " DESC";
+        String sortOrder = TicketsContract.TicketEntry.COLUMN_ID + " DESC";
 
-            cursor = db.query(
-                    TicketsContract.TicketEntry.TABLE_NAME,                     // The table to query
-                    projection,                               // The columns to return
-                    selection,                                // The columns for the WHERE clause
-                    selectionArgs,                            // The values for the WHERE clause
-                    null,                                     // don't group the rows
-                    null,                                     // don't filter by row groups
-                    sortOrder                                 // The sort order
-            );
+        cursor = db.query(
+                TicketsContract.TicketEntry.TABLE_NAME,                     // The table to query
+                projection,                               // The columns to return
+                selection,                                // The columns for the WHERE clause
+                selectionArgs,                            // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
 
-            cursor.moveToNext();
+        boolean isOkay = cursor.moveToNext();
+
+
+        if (!isOkay) {
+            Log.i("Ticket", "SHITS ABOUT TO HIT THE FAN!!!!!");
+            Toast t = Toast.makeText(getContext(), "This Ticket was not bought using this device, please use the correct device", Toast.LENGTH_LONG);
+            t.show();
+            finish();
+            Intent intent = new Intent(TicketDetailActivity.this, WalletActivity.class);
+            startActivity(intent);
+        } else {
+
             date = cursor.getLong(cursor.getColumnIndexOrThrow(TicketsContract.TicketEntry.COLUMN_DATE));
             departure = cursor.getString(cursor.getColumnIndexOrThrow(TicketsContract.TicketEntry.COLUMN_DEPARTURE));
             departureTime = cursor.getLong(cursor.getColumnIndexOrThrow(TicketsContract.TicketEntry.COLUMN_DEPARTURE_TIME));
@@ -92,70 +106,64 @@ public class TicketDetailActivity extends AppCompatActivity {
             qrContent = cursor.getString(cursor.getColumnIndexOrThrow(TicketsContract.TicketEntry.COLUMN_CODE_DTO));
             cursor.close();
 
-            Log.i("SQLITE","WENT THROUGH THE CURSOR");
 
-        } catch (SQLiteAbortException e){
-            Log.e("SQLLite:", e.getStackTrace().toString());
+            Log.i("SQLITE READING", "GOT READABLE DATABASE: departure = " + departure + "; destination = " + destination + "; date = " + date + "; price = " + price + "; qrcontent = " + qrContent + ";");
+
+            dateView = (TextView) findViewById(R.id.ticket_departure_date);
+            departureView = (TextView) findViewById(R.id.ticket_departure);
+            destinationView = (TextView) findViewById(R.id.ticket_destination);
+            priceView = (TextView) findViewById(R.id.ticket_price);
+
+            //Value attribution to each View
+            DateFormat df = new SimpleDateFormat("dd/MM/yy");
+            String departureDateString = df.format(new Date(date));
+
+            DateFormat tf = new SimpleDateFormat(" 'at' HH:mm");
+            String departureTimeString = tf.format(new Date(departureTime));
+
+            String departureDateTime = departureDateString + departureTimeString;
+
+            dateView.setText(departureDateTime);
+            departureView.setText(departure);
+            destinationView.setText(destination);
+
+            //TODO remove hardcoded currency values
+            priceView.setText(String.format("%.2f €", price));
+
+
+            qrCodeImageview = (ImageView) findViewById(R.id.img_qr_code_image);
+
+            Thread t = new Thread(new Runnable() {  // do the creation in a new thread to avoid ANR Exception
+                public void run() {
+                    final Bitmap bitmap;
+                    try {
+                        bitmap = encodeAsBitmap(qrContent);
+                        runOnUiThread(new Runnable() {  // runOnUiThread method used to do UI task in main thread.
+                            @Override
+                            public void run() {
+                                qrCodeImageview.setImageBitmap(bitmap);
+                            }
+                        });
+                    } catch (WriterException e) {
+                        errorMsg += "\n" + e.getMessage();
+                    }
+                    if (!errorMsg.isEmpty())
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                            }
+                        });
+                }
+            });
+            t.start();
         }
-
-        Log.i("SQLITE READING", "GOT READABLE DATABASE: departure = " + departure + "; destination = " + destination + "; date = " + date + "; price = " + price + ";" );
-
-        dateView = (TextView) findViewById(R.id.ticket_departure_date);
-        departureView = (TextView) findViewById(R.id.ticket_departure);
-        destinationView = (TextView) findViewById(R.id.ticket_destination);
-        priceView = (TextView) findViewById(R.id.ticket_price);
-
-        //Value attribution to each View
-        DateFormat df = new SimpleDateFormat("dd/MM/yy");
-        String departureDateString = df.format(new Date(date));
-
-        DateFormat tf = new SimpleDateFormat(" 'at' HH:mm");
-        String departureTimeString = tf.format(new Date(departureTime));
-
-        String departureDateTime = departureDateString + departureTimeString;
-
-        dateView.setText(departureDateTime);
-        departureView.setText(departure);
-        destinationView.setText(destination);
-
-        //TODO remove hardcoded currency values
-        priceView.setText(String.format("%.2f €", price));
-
-
-        qrCodeImageview=(ImageView) findViewById(R.id.img_qr_code_image);
-
-        Thread t = new Thread(new Runnable() {  // do the creation in a new thread to avoid ANR Exception
-            public void run() {
-                final Bitmap bitmap;
-                try {
-                    bitmap = encodeAsBitmap(qrContent);
-                    runOnUiThread(new Runnable() {  // runOnUiThread method used to do UI task in main thread.
-                        @Override
-                        public void run() {
-                            qrCodeImageview.setImageBitmap(bitmap);
-                        }
-                    });
-                }
-                catch (WriterException e) {
-                    errorMsg += "\n" + e.getMessage();
-                }
-                if (!errorMsg.isEmpty())
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                        }
-                    });
-            }
-        });
-        t.start();
     }
 
     Bitmap encodeAsBitmap(String str) throws WriterException {
         BitMatrix result;
         try {
             result = new MultiFormatWriter().encode(str, BarcodeFormat.QR_CODE, WIDTH, WIDTH, null);
-        }
-        catch (IllegalArgumentException iae) {
+        } catch (IllegalArgumentException iae) {
             // Unsupported format
             return null;
         }
@@ -165,7 +173,7 @@ public class TicketDetailActivity extends AppCompatActivity {
         for (int y = 0; y < h; y++) {
             int offset = y * w;
             for (int x = 0; x < w; x++) {
-                pixels[offset + x] = result.get(x, y) ? getResources().getColor(R.color.colorPrimary):getResources().getColor(R.color.white);
+                pixels[offset + x] = result.get(x, y) ? getResources().getColor(R.color.colorPrimary) : getResources().getColor(R.color.white);
             }
         }
         Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
